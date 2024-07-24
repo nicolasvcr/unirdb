904f51936bc962b51b72815b084aa0f851d0b06e
Nicolás Campos <yaca@localhost.localdomain>
2024-07-24 10:54:15 -0400

    unirdb function

---
+++ unirdb.R
@@ -0,0 +1,22 @@
+unirdb <- function(x,y) {
+    if (identical(names(x), names(y))) {
+        rbind(x, y)
+    } else if (all(names(x) %in% names(y)) & all(names(y) %in% names(x))){
+        print(" Los archivos tienen las mismas columnas, pero en distinto orden")
+        continuar <- TRUE
+        resp <- readline(prompt=" ¿Reordenar columnas? (s/n) ")
+        while (continuar){
+        if (resp == "s") {
+            continuar <- FALSE
+        } else if (resp == "n") {
+            stop("No se modificaron los archivos")
+        } else {
+            resp <- readline(prompt=" Responda sí (s) o no (n) ")
+            continuar <- TRUE
+        }
+        return(unirdb(x,y[names(x)]))
+    }
+    } else {
+        stop(" Los archivos no tienen las mismas columnas")
+    }
+}
